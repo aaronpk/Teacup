124f1d72717c8e8c3f667437d6a3dfe7d9ce881d
Aaron Parecki <aaron@parecki.com>
2015-04-19 09:53:27 -0700

    update wording

--- views/pebble-settings-login.php
+++ views/pebble-settings-login.php
@@ -4,7 +4,7 @@
   <h2>Sign in with your Domain</h2>
 
   <p>Enter your website below to sign in.</p>
-  <p>If your website supports Micropub, it will log posts directly to your site. Otherwise, it will post to your profile on this website.</p>
+  <p>If your website supports Micropub, it will log posts directly to your site. Otherwise, it will post to your profile on teacup.p3k.io.</p>
 
   <form action="/auth/start" method="get" class="form-inline">
     <input type="url" name="me" placeholder="http://me.com" value="" class="form-control">
